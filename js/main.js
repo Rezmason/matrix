f0f422e933a393df4e9f789d0438fc4ac167500d
Rezmason <jeremysachs@rezmason.net>
2021-11-09 18:26:00 -0800

    Only load one solution— regl, or webgpu, not both. This requires dynamic import.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
 import makeConfig from "./config.js";
-import initWebGPU from "./webgpu/main.js";
-import initREGL from "./regl/main.js";
 
 const canvas = document.createElement("canvas");
 document.body.appendChild(canvas);
@@ -8,12 +6,10 @@ document.addEventListener("touchmove", (e) => e.preventDefault(), {
 	passive: false,
 });
 
-document.body.onload = () => {
+document.body.onload = async () => {
 	const urlParams = Object.fromEntries(new URLSearchParams(window.location.search).entries());
+	const useREGL = navigator.gpu == null || ["webgl", "regl"].includes(urlParams.renderer?.toLowerCase());
+	const solution = import(`./${useREGL ? "regl" : "webgpu"}/main.js`);
 	const config = makeConfig(urlParams);
-	if (navigator.gpu == null || ["webgl", "regl"].includes(urlParams.renderer?.toLowerCase())) {
-		initREGL(canvas, config);
-	} else {
-		initWebGPU(canvas, config);
-	}
+	(await solution).default(canvas, config);
 };
