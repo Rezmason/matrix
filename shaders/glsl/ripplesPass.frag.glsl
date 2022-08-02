25255599aa9674d77b709a9dadd6074a69d4ca59
Rezmason <jeremysachs@rezmason.net>
2022-08-02 04:12:52 -0700

    Replaced the sudden sine ripple with a sin(x)/x offset by elapsed time.

--- shaders/glsl/ripplesPass.frag.glsl
+++ shaders/glsl/ripplesPass.frag.glsl
@@ -13,13 +13,13 @@ void main() {
 	for (int i = 0; i < 5; i++) {
 		vec3 click = clicks[i];
 		float distanceToClick = length((click.xy - vUV) * vec2(aspectRatio, 1.0));
-		total += (1.0 - distanceToClick)
-		* sin(distanceToClick * 50.0 - time * 12.0)
-		* pow(1.0 - min(1.0, (time - click.z) / 3.0), 2.0);
+		float elapsedTime = clamp(time - click.z, -100.0, 100.0);
+		float t = distanceToClick - elapsedTime * 0.5;
+		total += sin(t * 40.0) / t;
 	}
 	total *= 0.2;
 
-	vec2 uv = vUV + total * 0.03;
+	vec2 uv = vUV + total * 0.001;
 	gl_FragColor = vec4(mix(vec3(0.0), vec3(0.3, 1.0, 0.2), texture2D(tex, uv).r + texture2D(bloomTex, uv).r * 0.5), 1.0);
 	// gl_FragColor = vec4(uv, 0.5, 1.0);
 }
